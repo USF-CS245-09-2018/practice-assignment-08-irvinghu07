976e183554af913f6d46eed8e4da4b854b8f5353
irvinghu07 <rhu5@dons.usfca.edu>
2018-10-29 00:12:33 -0700

    LinkList.java file added

---
+++ src/LinkedList.java
@@ -0,0 +1,32 @@
+/**
+ * Development IDE: IntelliJ IDEA
+ * Author: irving
+ * Project Name: cs245-assignment-08
+ * Date: 2018-10-29
+ */
+public class LinkedList implements List {
+    @Override
+    public void add(Object obj) throws Exception {
+
+    }
+
+    @Override
+    public void add(int pos, Object obj) throws Exception {
+
+    }
+
+    @Override
+    public Object get(int pos) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Object remove(int pos) throws Exception {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
